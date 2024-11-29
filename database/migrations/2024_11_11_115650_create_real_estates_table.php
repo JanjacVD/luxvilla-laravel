50deb3cc7162f4b034ec6240dd891071e64976b1
Valentino Janjac <valentino.janjac@bozic.io>
2024-11-29 10:50:11 +0100

    Change db

--- database/migrations/2024_11_11_115650_create_real_estates_table.php
+++ database/migrations/2024_11_11_115650_create_real_estates_table.php
@@ -19,20 +19,27 @@ return new class extends Migration {
             $table->boolean('featured')->default(false);
             $table->foreignId('area_id')->references('id')->on('areas')->onDelete('cascade');
             $table->foreignId('estate_type_id')->references('id')->on('estate_types')->onDelete('cascade');
-            $table->double('price');
-            $table->double('area_size');
-            $table->string('year_built');
+            $table->double('price')->nullable();
+            ;
+            $table->double('area_size')->nullable();
+            ;
+            $table->string('year_built')->nullable();
             $table->string('year_renovated')->nullable();
             $table->boolean('parking_available')->default(false);
             $table->integer('parking_spaces')->nullable();
             $table->integer('rooms')->nullable();
             $table->json('description')->nullable();
-            $table->string('owner_name');
-            $table->string('owner_contact');
-            $table->string('address');
+            $table->string('owner_name')->nullable();
+            ;
+            $table->string('owner_contact')->nullable();
+            ;
+            $table->string('address')->nullable();
+            ;
             $table->date('sold_at')->nullable();
-            $table->json('coordinates');
-            $table->integer('radius');
+            $table->json('coordinates')->nullable();
+            ;
+            $table->integer('radius')->nullable();
+            ;
         });
     }
 
